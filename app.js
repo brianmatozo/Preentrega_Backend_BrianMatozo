c5aa456f17e0d58d57d9e0f1f5ddb604362ca21a
Brian Matozo <brianmatozo@gmail.com>
2024-05-26 23:15:19 -0300

    11

--- app.js
+++ app.js
@@ -50,19 +50,19 @@ app.get('/', (req, res) => {
 
 // Socket.io setup
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('Usuario conectado');
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        console.log('Usuario desconectado');
     });
 
     // Custom event handlers can be added here
     socket.on('message', (msg) => {
-        console.log('Message received: ', msg);
+        console.log('mensaje: ', msg);
         io.emit('message', msg); // Broadcast to all clients
     });
 });
 
 // server
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`server en http://localhost:${PORT}`);
 });
